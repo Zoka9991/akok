3b05a2c65aa7572e3268b6d416a0563cf690e4dd
Zoka9991 <46074982+Zoka9991@users.noreply.github.com>
2018-12-25 19:17:22 +0200

    Add files via upload

---
+++ bot (1).js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By ExoDia");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("")
+setInterval(function() {
+channel.send(`ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches ExoDia Is Here Bitches`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
